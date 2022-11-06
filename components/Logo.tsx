9a1e39c48c1cb64a49eae00c53d29de1c9b63020
Cut Meurah Rudi <cut@circles.asia>
2022-11-06 21:33:37 +0800

    update svg

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ ...rest }) {
       </g>
       <g filter="url(#filter1_d_122_33)">
         <circle cx="137.631" cy="86" r="5.5" transform="rotate(3.62274 137.631 86)" fill="url(#paint0_linear_122_33)" stroke="url(#paint1_linear_122_33)" strokeWidth="3"/>
-        <path d="M50.1309 123.5L67.6309 93L85.7283 61.5677C86.1207 60.8861 87.1099 60.9034 87.4783 61.5981L104.131 93L120.199 121.829C120.593 122.534 121.617 122.507 121.971 121.781L132.131 101" stroke="url(#paint2_linear_122_33)" strokeWidth="10" stroke-linecap="round"/>
+        <path d="M50.1309 123.5L67.6309 93L85.7283 61.5677C86.1207 60.8861 87.1099 60.9034 87.4783 61.5981L104.131 93L120.199 121.829C120.593 122.534 121.617 122.507 121.971 121.781L132.131 101" stroke="url(#paint2_linear_122_33)" strokeWidth="10" strokeLinecap="round"/>
       </g>
       <defs>
         <filter id="filter0_b_122_33" x="-40" y="-40" width="260" height="260" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
