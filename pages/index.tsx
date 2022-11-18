ca494d8f2f56d6c434faf34729af8b7f95cf3b4a
Cut Meurah Rudi <cut@circles.asia>
2022-11-18 23:07:29 +0800

    Add new product.svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           {/*<Partners />*/}
-          <BasicSection imageUrl="/finance.svg" title="Lebih Bagus ada Headline" overTitle="Tentang Kami">
+          <BasicSection imageUrl="/new-product.svg" title="Lebih Bagus ada Headline" overTitle="Tentang Kami">
             <p>
               Terdiri dari orang-orang berpengalaman di perusahaan teknologi di Asia Tenggara.
               Aigov dapat mengembangkan sistem dan tampilan yang komprehensif dan sesuai dengan kubutuhan masing-masing sektor pemerintahan.
